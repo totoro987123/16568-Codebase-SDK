8c1e3c7c1b71be15332f56d1a3b17aeeea3817fc
lawsonwright <lawsonwright@icloud.com>
2020-10-21 17:25:02 -0400

    Added ControlerInputEventHandler to the codebase to handle gamepad events.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/events/controlerEvents/ButtonPressDown.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.events.controlerEvents;
-
-import org.firstinspires.ftc.teamcode.events.Event;
-
-public class ButtonPressDown implements Event {
-    public boolean newValue;
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ControlerInputEventHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ControlerInputEventHandler.java
@@ -1,12 +1,60 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.events.Event;
 import org.firstinspires.ftc.teamcode.settings.GlobalVariables;
+
+import java.util.HashMap;
 
 public class ControlerInputEventHandler {
 
+    private final String eventsClassPath = "org.firstinspires.ftc.teamcode.events.controlerEvents.";
+
     private GlobalVariables globalVariables;
 
-    public ControlerInputEventHandler() {
+    private String[] inputNamesBoolean = {"a", "b", "back", "dpad_down", "dpad_left", "dpad_right", "dpad_up", "left_bumper", "left_stick_button", "right_bumper", "right_stick_button", "x", "y"};
+    private HashMap<String, Boolean> booleanInputStore = new HashMap<String, Boolean>();
 
+    public ControlerInputEventHandler(GlobalVariables globalVariables) {
+        this.globalVariables = globalVariables;
+    }
+
+    private void updateGamepadStatus(Gamepad gamepad) {
+        Object object = (Object) gamepad;
+        Class<?> gamepadClass = object.getClass();
+
+        for (String variableName : this.inputNamesBoolean) {
+            try {
+                boolean lastValue = booleanInputStore.get(variableName);
+                boolean currentValue = (boolean) gamepadClass.getField(variableName).get(gamepad);
+
+                if ((lastValue != currentValue) && booleanInputStore.containsKey(variableName)) {
+                    if (currentValue) {
+                        Class<?> eventClass = Class.forName(this.eventsClassPath + "Button" + variableName.toUpperCase() + "Press");
+                        Event event = (Event) eventClass.newInstance();
+                        this.globalVariables.eventBus.post(event);
+                    } else {
+                        Class<?> eventClass = Class.forName(this.eventsClassPath + "Button" + variableName.toUpperCase() + "Lift");
+                        Event event = (Event) eventClass.newInstance();
+                        this.globalVariables.eventBus.post(event);
+                    }
+                }
+                this.booleanInputStore.put(variableName, currentValue);
+
+            } catch (NoSuchFieldException e) {
+                System.out.println(e.toString());
+            } catch (SecurityException e) {
+                System.out.println(e.toString());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.toString());
+            } catch (IllegalAccessException e) {
+                System.out.println(e.toString());
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.toString());
+            } catch (InstantiationException e) {
+                System.out.println(e.toString());
+            }
+        }
     }
 }
