3ec22fc11b2b2b094a25fb1aacfc057a82938967
lawsonwright <lawsonwright@icloud.com>
2020-10-22 08:44:26 -0400

    Add update() to the ControlerInputEventHandler Added registation of the controlers so they can listen to events.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controlers/TestControler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controlers/TestControler.java
@@ -4,18 +4,24 @@ import com.google.common.eventbus.Subscribe;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.events.controlerEvents.Button_gamepad1_A_Lift;
 import org.firstinspires.ftc.teamcode.events.controlerEvents.Button_gamepad1_A_Press;
 import org.firstinspires.ftc.teamcode.settings.GlobalVariables;
 
 public class TestControler extends Controler {
 
-    public TestControler(GlobalVariables globalVariables){
+    private boolean pressed = false;
+    private Telemetry telemetry;
+
+    public TestControler(GlobalVariables globalVariables, Telemetry telemetry){
         super.globalVariables = globalVariables;
+        this.telemetry = telemetry;
     }
 
     @Override
     public void update() {
-
+        telemetry.addData("Button A Press: ", "Button A Press: " + String.valueOf(this.pressed));
     }
 
     @Override
@@ -25,5 +31,15 @@ public class TestControler extends Controler {
 
     @Override
     public void stop() {
+    }
+
+    @Subscribe
+    public void buttonAPress(Button_gamepad1_A_Press event) {
+        this.pressed = true;
+    }
+
+    @Subscribe
+    public void buttonALift(Button_gamepad1_A_Lift event) {
+        this.pressed = false;
     }
 }
